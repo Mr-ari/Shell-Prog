21fba84e46ecd100503ba890c3a0d5e70a72d71e
Mr-ari <arijitghosh1106@gmail.com>
2018-02-21 11:52:27 +0530

    econd push

--- ass_2.sh
+++ ass_2.sh
@@ -1,3 +1,7 @@
 #greatest of three numbers
 
-read -p 
+read -p "Enter the first number- " num1
+read -p "Enter the second number- " num2
+read -p "Enter the third number- " num3
+
+if test num1 -gt num2  
